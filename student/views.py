55e838c1913139d7454a7b653f625c50ef0c5c4d
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-07-23 19:43:43 +0530

    7th commit

--- student/views.py
+++ student/views.py
@@ -4,20 +4,21 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
-    if request.method=='POST':
-        designation = request.POST['designation']
-        user=authenticate(
-        username=request.POST['username'],
-        password=request.POST['password']
-        )
-        print(designation)
-        # print(email)
-        # print(password)
-        if user is not None:
-            login(request,user)
-            return HttpResponse('<h1 style="color:green;"> Login Sucessful</h1>')
-    else:
+    try:
+        if request.method=='POST':
+            designation = request.POST['designation']
+            user=authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+            )
+            if user is not None:
+                login(request,user)
+                return HttpResponse('<h1 style="color:green;"> Login Sucessful</h1>')
+        else:
+            return render(request,'index.html')
+    except:
         return render(request,'index.html')
+
 
 def home(request):
     return render(request,'home.html')
