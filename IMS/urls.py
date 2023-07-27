53eedd9d8cfe410648f28da8e0a3fb1207eac9dc
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-07-27 14:20:52 +0530

    navbar

--- IMS/urls.py
+++ IMS/urls.py
@@ -21,6 +21,7 @@ from student import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
+    path('sru',views.index,name="sru"),
     path('login',views.my_login,name="login"),
     path('home',views.home,name="home"),
     path('tpo_dashboard',views.tpo_dashboard,name="tpo_dashboard"),
