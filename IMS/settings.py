85f0831f69bd65a642c279f75b71aee5fa9ccae0
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-07-28 14:00:55 +0530

    new dash

--- IMS/settings.py
+++ IMS/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/IMS/IMS/assets'
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATIC_ROOT = '/IMS/IMS/assets'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 # Default primary key field type
