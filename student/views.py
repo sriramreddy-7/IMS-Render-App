10ac506cb60a95194637f585733f2dce05607dc5
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-07-30 12:11:02 +0530

    all btech 25

--- student/views.py
+++ student/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth import authenticate,logout,login
 from django.db.models import Count
 from django.contrib.auth.models import User
-from student.models import Jobs,Std,SD2
+from student.models import Jobs,Std,SD2,btech_25
 from django.http import HttpResponse,HttpResponseRedirect
 from django.views.decorators.cache import cache_control
 from django.contrib.auth.decorators import login_required
-# import pandas as pd
+import pandas as pd
 # Create your views here.
 def index(request):
     return render(request,'login.html')
@@ -78,9 +78,11 @@ def student_dashboard(request):
 def data_store(request):
     # csv_file_path = r"C:\Users\sriramreddykoonadi\Project\IMS\IMS\CSE_25_DB.csv"
     # csv_file_path = r"C:\Users\sriramreddykoonadi\Downloads\SD-2_EZ.csv"
-    # df = pd.read_csv(csv_file_path)
-    # print(df.columns)
-    # print(df.dtypes)
+    csv_file_path=r"C:\Users\sriramreddykoonadi\OneDrive - SR University\Bachelor of Technology\Database\References'\B.TECH_ALL_DEPTS_25(1).csv"
+    df = pd.read_csv(csv_file_path)
+    print(df.columns)
+    print(df.dtypes)
+    print(df.head())
 
     '''for index, row in df.iterrows():
         hall_ticket_no = row['HALLTICKET_NO']
@@ -115,30 +117,32 @@ def data_store(request):
         )
         obj.save()'''
     '''for index, row in df.iterrows():
-        hall_ticket_no = row['Roll No']
-        student_name = row['Student Name']
-        branch = row['Branch']
-        section = row['SD-2']
-        email_id = row['Email']
-        venue=row['Venue']
+        hall_ticket_no = row['H. T. No.']
+        student_name = row['Name of the Student']
+        branch = row['Dept.']
+        
+        # section = row['SD-2']
+        email_id = row['Email Id']
+        # venue=row['Venue']
 
         # Adjust and format the data as needed
-        student_name = student_name.title().strip()  # Capitalize student name and remove leading/trailing spaces
-        branch = branch.strip()
-        hall_ticket_no=hall_ticket_no.strip()
-        section=section.strip()
-        email_id=email_id.strip()
-        venue=venue.strip()
-        print(hall_ticket_no)
-        obj = SD2(
+        student_name = str(student_name).title().strip()  # Capitalize student name and remove leading/trailing spaces
+        branch = str(branch).strip()
+        hall_ticket_no=str(hall_ticket_no).strip()
+        # section=section.strip()
+        email_id=str(email_id).strip()
+        # venue=venue.strip()
+        
+        obj = btech_25(
             hall_ticket_no=hall_ticket_no,
             student_name=student_name,
             branch=branch,
-            training_type=section,
-            venue=venue,
+            # training_type=section,
+            # venue=venue,
             email_id=email_id,
         )
         obj.save()
+        print(f"{hall_ticket_no} Added to database!")'''
         
     # for index, row in df.iterrows():
     #     obj = Std(
@@ -149,7 +153,6 @@ def data_store(request):
     #         email_id=row['EMAIL_ID'],
     #     )
     #     obj.save()   '''
-        
     
     return HttpResponse('<h1 style="color:green;"> Details has been stored succesfully into the database !</h1>')
 
@@ -180,3 +183,8 @@ def all(request):
 def job_listing(request):
     jobs=Jobs.objects.all()
     return render(request,'job_listing.html',{'jobs':jobs})
+
+
+def all_btech_25(request):
+    std=btech_25.objects.all()
+    return render(request,'btech_25.html',{'std':std})
